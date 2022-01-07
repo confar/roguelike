cefbe602dd6a10a8b91c306862f9a3410bca54e0
confar <tentigers@mail.ru>
2022-01-07 23:52:44 +0300

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -311,7 +311,7 @@ fn main() {
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("Rust libctod tutorial")
+        .title("Rust libtcod tutorial")
         .init();
     tcod::system::set_fps(LIMIT_FPS);
     let mut tcod = Tcod {
